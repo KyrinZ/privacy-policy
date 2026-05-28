73dd74bbf513e3e9fc39a2c48e30fbf13a77ebbb
Bayajit Mohammed <bayajitmohammed@gmail.com>
2026-05-28 20:09:19 +0600

    feat: add GitHub Actions workflow for deploying to GitHub Pages and update Astro configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,17 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
-import vue from '@astrojs/vue';
+import vue from "@astrojs/vue";
 
-import tailwindcss from '@tailwindcss/vite';
+import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://kyrinz.github.io",
+  base: "/privacy-policy-generator",
   integrations: [vue()],
 
   vite: {
-    plugins: [tailwindcss()]
-  }
+    plugins: [tailwindcss()],
+  },
 });
